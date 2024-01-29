8f7324a94b6cfa98ca87a649e9344ab8219282d6
ericp <137734293+yaahboii@users.noreply.github.com>
2024-01-29 03:19:53 -0500

    Update NewClass.java

--- src/main/java/com/mycompany/csc229_assignment01/NewClass.java
+++ src/main/java/com/mycompany/csc229_assignment01/NewClass.java
@@ -1,14 +1,55 @@
 
+
 package com.mycompany.csc229_assignment01;
 
 /**
  *
- * @author MoaathAlrajab
+ * @author Eric Paiz
  */
-public class NewClass {
-    
-     public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Course {
+    // private members
+    private int ID;
+    private String Name;
+    private String Code;
+
+    // Default constructor
+    public Course() {
+
     }
-    
+// overloaded constructor
+
+    public Course(int ID, String Name, String Code) {
+        this.ID = ID;
+        this.Name = Name;
+        this.Code = Code;
+    }
+
+    // setters aka mutator methods
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public void setCode(String Code) {
+        this.Code = Code;
+    }
+
+// getters aka accessor methods
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getCode() {
+        return Code;
+    }
+
+
 }
